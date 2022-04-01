c0978fc6f89b1f30e3f69d25cca53a4133529611
Sean Overton <sean14o8.so@gmail.com>
2022-04-01 11:12:19 +1100

    Added allowed host in settings

--- myapi/settings.py
+++ myapi/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "csit314-python-backend.herokuapp.com"
+]
 
 
 # Application definition
