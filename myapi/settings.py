102868c06177af637aeaa54f7106e5bb065dc61f
Sean Overton <sean14o8.so@gmail.com>
2022-04-02 13:36:04 +1100

    Allowed csrf requests from backend heroku URL

--- myapi/settings.py
+++ myapi/settings.py
@@ -98,6 +98,9 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://csit314-python-backend.herokuapp.com'
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
