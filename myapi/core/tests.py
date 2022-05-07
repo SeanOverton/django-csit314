9ad319355fbfee02c339299c0c85868cb1eccd09
Sean Overton <sean14o8.so@gmail.com>
2022-05-07 18:17:14 +1000

    Started creating test data script

--- myapi/core/tests.py
+++ myapi/core/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import RoadsideCallout
 
-# Create your tests here.
+class CalloutTestCase(TestCase):
+    def setUp(self):
+        RoadsideCallout.objects.create(username="test", 
+                                        status="roar",
+                                        location="an address",
+                                        description="description")
+
+    def test_callouts_have_correct_data(self):
+        callout = RoadsideCallout.objects.get(username="test")
+        self.assertEqual(callout.status, "roar")
+        self.assertEqual(callout.location, "an address")
+        self.assertEqual(callout.description, "description")
+
+    # maybe this should be a serializer test seperate from the models test
+    def test_callouts_update_correctly(self):
+        callout = RoadsideCallout.objects.get(username="test")
+        self.assertEqual(callout.status, "roar")
+        self.assertEqual(callout.location, "an address")
+        self.assertEqual(callout.description, "description")
